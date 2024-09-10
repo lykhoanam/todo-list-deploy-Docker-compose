f3e431c2136189edd1c5960b637531405402467a
Ajeet Singh Raina <ajeetraina@gmail.com>
2024-09-11 02:03:48 +0530

    fixed the Mongo connectivity

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB using the environment variable
-mongoose.connect('mongodb://localhost:27017/todos')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todos', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Error connecting to MongoDB:', error));
 
